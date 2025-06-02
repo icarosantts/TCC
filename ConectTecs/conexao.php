1fd8712053c8690f95ae959ee675e20765fcf68d
Icaro Santos <icaro.a.santos1227@gmail.com>
2025-06-01 21:22:02 -0300

    Update conexao.php

--- ConectTecs/conexao.php
+++ ConectTecs/conexao.php
@@ -1,14 +1,16 @@
 <?php
 
-$servername = "sql305.infinityfree.com"; // ou o que aparecer no painel
-$username = "if0_39002126";      // substitua pelo seu
-$password = "BCBn1iRjGzI";     // senha definida por você
-$dbname = "if0_39002126_conecttecs_db"; // nome completo do banco
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "conecttecs_db";
 
+// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verifica a conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
+
 ?>
